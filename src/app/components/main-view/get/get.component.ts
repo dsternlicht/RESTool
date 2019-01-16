83ab4a9ff128031447ed7f556b11b6da8f6016eb
Jesse McDowell <jesse.mcdowell@aquaticinformatics.com>
2019-01-16 10:28:40 -0800

    add sortBy option for GET All page

--- src/app/components/main-view/get/get.component.ts
+++ src/app/components/main-view/get/get.component.ts
@@ -84,6 +84,15 @@ export class GetComponent {
         this.loading = false;
         this.data = this.dataPathUtils.extractDataFromResponse(data, this.activeGetRequest.dataPath);
 
+        const sortBy = this.activeGetRequest.display.sortBy;
+        if (sortBy) {
+          this.data.sort((a, b) => {
+            const aProperty = this.dataPathUtils.extractDataFromResponse(a, null, sortBy);
+            const bProperty = this.dataPathUtils.extractDataFromResponse(b, null, sortBy);
+            return aProperty < bProperty ? -1 : aProperty > bProperty ? 1 : 0;
+          })
+        }
+
         if (environment.logApiData) {
           console.log('Got data after dataPath: ', this.data);
         }
