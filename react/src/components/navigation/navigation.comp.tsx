791bb747b9a7778b8ce8afd30890f2aa855f6fee
Daniel Sternlicht <gdblogst@gmail.com>
2020-01-04 20:34:59 +0200

    Adding mobile support! (!!!)

--- react/src/components/navigation/navigation.comp.tsx
+++ react/src/components/navigation/navigation.comp.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { IAppContext } from '../app.context';
 import { withAppContext } from '../withContext/withContext.comp';
+import { Button } from '../button/button.comp';
 
 import './navigation.scss';
 
@@ -11,13 +12,27 @@ interface IProps {
 }
 
 const NavigationComp = ({ context: { config } }: IProps) => {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
   return (
     <nav className="app-nav">
-      {
-        (config?.pages || []).map((page, idx) => (
-          <NavLink to={`/${page.id || idx + 1}`} activeClassName="active" key={`page_${idx}`}>{page.name}</NavLink>
-        ))
-      }
+      <Button className="app-nav-opener" onClick={() => setIsOpened(!isOpened)}>
+        {
+          isOpened ? 
+          <i className="fa fa-times" aria-hidden="true"></i> : 
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        }
+      </Button>
+
+      <div className={`app-nav-wrapper ${isOpened ? 'opened' : ''}`}>
+        <div className="app-nav-links">
+          {
+            (config?.pages || []).map((page, idx) => (
+              <NavLink to={`/${page.id || idx + 1}`} activeClassName="active" key={`page_${idx}`} onClick={() => setIsOpened(false)}>{page.name}</NavLink>
+            ))
+          }
+        </div>
+      </div>
     </nav>
   );
 }
