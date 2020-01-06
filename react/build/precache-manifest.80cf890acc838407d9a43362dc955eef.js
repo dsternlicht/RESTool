50e68b80dcc44a71b42c135b787c2ff0d32d5a0b
Daniel Sternlicht <gdblogst@gmail.com>
2020-01-06 17:23:30 +0200

    Adding dist folder

---
+++ react/build/precache-manifest.80cf890acc838407d9a43362dc955eef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924550a77497c0ac1483a6d972af8dae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d86ef6ebb13b448994f",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "28acf8f7cd5649044104",
+    "url": "/static/css/main.6a626506.chunk.css"
+  },
+  {
+    "revision": "4d86ef6ebb13b448994f",
+    "url": "/static/js/2.d3431236.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.d3431236.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28acf8f7cd5649044104",
+    "url": "/static/js/main.9b60035e.chunk.js"
+  },
+  {
+    "revision": "720f01932bf7eee340af",
+    "url": "/static/js/runtime-main.8b544fbb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
