e658f9cb054a4a142c9df7ac8a0c54e7a3e4af8b
Daniel Sternlicht <gdblogst@gmail.com>
2020-01-08 15:13:09 +0200

    Completely removing old angular app

--- src-backup/app/components/main-view/main-view.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from '@angular/router';
-import { MainViewComponent } from './main-view.component';
-
-const routes = [
-  {path: '', component: MainViewComponent},
-  {path: ':pageId', component: MainViewComponent}
-];
-
-export const Routing = RouterModule.forChild(routes);
