a355f4cefbbeefc98b1a50ceb279b7440745d42b
Daniel Sternlicht <gdblogst@gmail.com>
2020-05-06 13:07:27 +0300

    Bumping to 2.1.0, building.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea60ae01cab7cca994cab2475fda1086.js"
+  "./precache-manifest.7f4fa07540214030ea23532cf7578906.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
