9088d345956acb727a5298b20d2f9f262b164a89
fesath <febin.sathar@medlife.com>
2020-05-27 22:20:38 +0530

    resolved review comments

--- build/server/routes/ping/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const pingServer = express_1.Router();
-pingServer
-    .get('/ping', (req, res) => {
-    res.status(200).json({ 'ping': 'pong', 'version': process.env.npm_package_version });
-});
-exports.default = pingServer;
