d80d1279f1ae5d974f2ae8edf73c6bf831edb97a
martinb <martinbraun123@aol.com>
2020-07-10 13:54:47 +0200

    build after changes to add json-body

---
+++ build/precache-manifest.070ace9e353e762114d6e7c4d7b9c913.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb09b7e24ad7a24eafa4a3d27c896a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e2ca4e348022b1ce81b",
+    "url": "./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5592d26835af17809726",
+    "url": "./static/css/main.adfac819.chunk.css"
+  },
+  {
+    "revision": "0e2ca4e348022b1ce81b",
+    "url": "./static/js/2.155413fb.chunk.js"
+  },
+  {
+    "revision": "6ceee2230a275e6f350d0faebed10188",
+    "url": "./static/js/2.155413fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5592d26835af17809726",
+    "url": "./static/js/main.b234d68e.chunk.js"
+  },
+  {
+    "revision": "58d6219fe1f60f2fa5af",
+    "url": "./static/js/runtime-main.f1c188a0.js"
+  }
+]);
